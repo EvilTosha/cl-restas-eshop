73ac83fa1d25d6a624544541e48a944300779e74
Samoylov Anton <toshaevil@gmail.com>
2012-04-09 17:25:36 +0400

    some more changes

--- src/packages.lisp
+++ src/packages.lisp
@@ -11,15 +11,16 @@
           :cl-fad)
   (:import-from :arnesi :parse-float)
   (:import-from :alexandria :read-file-into-string)
-  (:export :compile-templates
-		   ;; :*storage*
-           :name
-           :unserialize
-           :plist-representation
-           :*path-to-bkps*
-           :*path-to-pics*
-           :*path-to-conf*
-           :*path-to-tpls*))
+  (:export :*eshop-config*
+					 :config.parse-config
+					 :compile-templates
+					 :name
+					 :unserialize
+					 :plist-representation
+					 :*path-to-bkps*
+					 :*path-to-pics*
+					 :*path-to-conf*
+					 :*path-to-tpls*))
 
 
 (defpackage #:wolfor-stuff
@@ -31,69 +32,3 @@
         #:eshop
         #:wolfor-stuff))
 
-
-
-;; (defpackage #:product
-;;   (:use #:cl
-;;         ;; #:cl-user
-;;         #:json
-;;         #:split-sequence)
-;;   (:export
-;;    ;; #:product
-;;    ;; #:articul
-;;    :parent
-;;    ;; #:name
-;;    ;; #:realname
-;;    :price
-;;    ;; #:siteprice
-;;    ;; #:ekkprice
-;;    :active
-;;    ;; #:newbie
-;;    ;; #:sale
-;;    ;; #:descr
-;;    ;; #:shortdescr
-;;    ;; #:count-transit
-;;    ;; #:count-total
-;;    :options
-;;    ;; #:plist-representation
-;;    ;; ;; Отображение
-;;    ;; #:get-pics
-;;    ;; #:show
-;;    ;; #:view
-;;    ;; #:serialize
-;;    ;; #:unserialize
-;;    )
-;;   )
-
-;; (defpackage #:optgroup
-;;   (:use #:cl
-;;         #:cl-user
-;;         #:json)
-;;   (:export
-;;    ;; #:optgroup
-;;    ;; #:show
-;;    ;; #:serialize
-;;    #:name
-;;    #:options
-;;    ;; #:unserialize
-;;    ))
-
-;; (defpackage #:option
-;;   (:use #:cl
-;;         #:cl-user
-;;         #:json)
-;;   (:export
-;;    ;; #:option
-;;    ;; #:show
-;;    #:name
-;;    #:value
-;;    ;; #:optype
-;;    ;; #:boolflag
-;;    ;; #:serialize
-;;    ;; #:unserialize
-;;    ))
-
-;; (defpackage #:optlist
-;;   (:use #:cl
-;;         #:cl-user)
-;;   (:export #:optlist))
